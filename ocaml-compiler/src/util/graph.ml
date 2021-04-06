8f3721a69f5f51fa83cd9bfb19e7f456819c353e
ryotaro.kasuga <krkr9893@gmail.com>
2021-04-06 15:22:03 +0900

    WIP

--- ocaml-compiler/src/util/graph.ml
+++ ocaml-compiler/src/util/graph.ml
@@ -33,17 +33,17 @@ let node_preprocess g a =
     Hashtbl.add g.redges a (Hashtbl.create 2);
   end
 
-let get_represent g a = Hashtbl.find g.belong a
+let represent g a = Hashtbl.find g.belong a
 
 let add_edge_aux g src dst =
-  let src = get_represent g src in
-  let dst = get_represent g dst in
+  let src = represent g src in
+  let dst = represent g dst in
   Hashtbl.replace (Hashtbl.find g.edges src) dst ();
   Hashtbl.replace (Hashtbl.find g.redges dst) src ()
 
 let rm_edge_aux g src dst =
-  let src = get_represent g src in
-  let dst = get_represent g dst in
+  let src = represent g src in
+  let dst = represent g dst in
   if (Hashtbl.mem g.nodes src) and (Hashtbl.mem g nodes dst) then begin
     Hashtbl.remove (Hashtbl.find g.edges src) dst;
     Hashtbl.remove (Hashtbl.find g.redges dst) src
@@ -70,57 +70,48 @@ let rm_edge g src dst =
   end
 
 let rm_node g a =
-  let a = get_represent g.belong a in
-  let lis = list_of_tblkey (Hashtbl.find g.edge a) in
-  List.iter (rm_edge g a) lis;
+  let a = represent g.belong a in
+  let lis = list_of_tblkey (Hashtbl.find g.edges a) in
+  let grp = list_of_tblkey (Hashtbl.find g.group a) in
   g.len <- g.len - 1;
+  List.iter (rm_edge g a) lis;
   Hashtbl.remove g.edges a;
   Hashtbl.remove g.redges a;
-  Hashtbl.iter (fun x _ -> Hashtbl.remove g.belong x) (Hashtbl.find g.group a);
-  Hashtbl.remove g.group a
+  Hashtbl.remove g.group a;
+  List.iter (Hashtbl.remove g.belong) grp
 
 let succ g a =
-  let a = get_represent g.belong a in
+  let a = represent g.belong a in
   list_of_edge (Hashtbl.find g.edges a)
 
 let pred g a = 
-  let a = get_represent g.belong a in
+  let a = represent g.belong a in
   list_of_edge (Hashtbl.find g.redges a)
 
 let length g = Hashtbl.length g.nodes
 
-let nodes g =
-  let tbl = Hashtbl.create (Hashtbl.length g.belong) in
-  let ns = list_of_tblkey g.belong in
-  let rec aux lis = match lis with
-    | x :: xs ->
-        if Hashtbl.mem tbl x then
-          aux xs
-        else begin
-          Hashtbl.replace tbl x ();
-          x :: (aux xs)
-        end
-    | [] -> []
-  in
-  aux ns
+let nodes g = list_of_tblkey g.group
 
 let degree g a =
-  let a = get_represent g a in
+  let a = represent g a in
   Hashtbl.length (Hashtbl.find g.edges a)
 
 let has_edge g src dst =
-  let src = get_represent g src in
-  let dst = get_represent g dst in
+  let src = represent g src in
+  let dst = represent g dst in
   Hashtbl.mem g.edges (src, dst)
 
-let add_edge_set g src dstset =
-  let src = get_represent g src in
-  Hashtbl.iter (fun x _ -> add_edge g src (get_represent g x)) dstset
+let contract_aux g a b =
+  let neig = list_of_tblkey (Hashtbl.find g.edges b) in
+  let grp = list_of_tblkey (Hashtbl.find g.group b) in
+  rm_node g b;
+  List.iter (fun x -> Hashtbl.replace g.belong x a) grp;
+  List.iter (fun x -> Hashtbl.replace (Hashtbl.find group a) x ()) grp;
+  List.iter (add_edge g a) neig
 
-(* FIXME subete *)
 let contraction g a b =
-  let a = get_represent g a in
-  let b = get_represent g b in
+  let a = represent g a in
+  let b = represent g b in
   if a = b then
     ()
   else begin
@@ -129,10 +120,7 @@ let contraction g a b =
       let bl = Hashtbl.length (Hashtbl.find g.edges b) in
       if al < bl then (b, a) else (a, b)
     in
-    let gr = Hashtbl.find g.group b in
-    List.iter (fun x -> add_edge_set g a (Hashtbl.find g.edges x))
-    List.iter (add_edge g a) edges;
-    rm_node g b
+    contract_aux g a b
   end
 
 let dump g f =
